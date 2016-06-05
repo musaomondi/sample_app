22a2b2ab216a97dfe2121a45cf9eff6252897d0f
musaomondi <amokem@yahoo.com>
2016-06-05 15:34:44 +0300

    user login and logout validated

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,13 +2,17 @@ class SessionsController < ApplicationController
   def new
   end
   def create
-  	user = User.find_by(email: params[:session][:email].downcase)
-  	if user && user.authenticate(params[:session][:password])
-  	else
-  		flash.now[:danger] ='Invalid email/password combination'
-    	render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+        log_in user
+        redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
   end
 end
   def destroy
+    log_out
+    redirect_to root_url
   end
 end
