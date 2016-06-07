02db804bddb4ef849357f51c51ba44fb55ebd1eb
musaomondi <amokem@yahoo.com>
2016-06-07 11:36:45 +0300

    creating edit form and testing successful and unsuccessful edits

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,21 @@ class UsersController < ApplicationController
   		render 'new'
   	end
   end
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user=User.find_by(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+      #Handle a successful update
+    else
+      render 'edit'
+    end
+  end
+
   private
   def user_params
   	params.require(:user).permit(:name, :email, :password, :password_confirmation)
