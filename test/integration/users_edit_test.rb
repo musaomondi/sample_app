5e046a7984612ec7000a0c39620d7d8320e3843f
musaomondi <amokem@yahoo.com>
2016-06-07 16:55:38 +0300

    Adding using authorizations

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -5,12 +5,14 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 		@user = users(:musa)
 	end
 	test "unsuccessful edit" do
+		log_in_as(@user)
 		get edit_user_path(@user)
 		assert_template 'users/edit'
 		patch user_path(@user), user: {name: "", email: "foo@invalid", password: "foo", password_confirmation: "bar"}
 		assert_template 'users/edit'
 	end
 	test "successful edit" do 
+		log_in_as(@user)
 		get edit_user_path(@user)
 		assert_template 'users/edit'
 		name = "Foo Bar"
