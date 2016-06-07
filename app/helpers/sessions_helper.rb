708db90b5073e5f8bfd6bf3c25c380fe093da35d
musaomondi <amokem@yahoo.com>
2016-06-07 10:01:04 +0300

    adding remember me checkbox and forgetting users

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,7 +6,7 @@ end
 #remembers a user in a persistent session.
 def remember(user)
 	user.remember
-	cookies.permanent.signed[:user_id]=user_id
+	cookies.permanent.signed[:user_id]=user.id
 	cookies.permanent[:remember_token]=user.remember_token
 end
 # Returns the current logged-in user (if any).
@@ -25,7 +25,14 @@ def current_user
  def logged_in?
  	!current_user.nil?
  end
+ #Forgets a persistent session.
+ def forget(user)
+ 	user.forget
+ 	cookies.delete(:user_id)
+ 	cookies.delete(:remember_token)
+ end
  def log_out
+ 	forget(current_user)
 	session.delete(:user_id)
 	@current_user = nil
  end
