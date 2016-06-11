fe60202f3a9ee19b610943644a419a06db11bf02
musaomondi <amokem@yahoo.com>
2016-06-11 09:49:52 +0300

    adding a test to make sure one user cant delete the microposts for another

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -17,5 +17,13 @@ class MicropostsControllerTest < ActionDispatch::IntegrationTest
   	end
 	assert_redirected_to login_url
 	end
+  test "should redirect destroy when wrong micropost" do
+    log_in_as(users(:michael))
+    micropost = microposts(:ants)
+    assert_no_difference 'Micropost.count' do
+      delete micropost_path :destroy, id: micropost
+    end
+    assert_redirected_to root_url
+  end
   end
 
