4b4f73e16562349fe0c87fe2273f0a9932941b54
musaomondi <amokem@yahoo.com>
2016-06-12 16:55:11 +0300

    refactoring Home page to use seperate partials

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -9,7 +9,7 @@ class Micropost < ApplicationRecord
   private
   #validates the size of an uploaded picture
   def picture_size
-  	if picture_size > 5.megabytes
+  	if picture.size > 5.megabytes
   		errors.add(:picture, "should be less than 5MB")
   	end
   end
