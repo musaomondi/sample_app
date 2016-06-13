9da9f2b262d291337152858d1b9285256926da02
musaomondi <amokem@yahoo.com>
2016-06-13 06:33:02 +0300

    apply the changes by reseting the database to reseed the data

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
 
 	has_many :microposts
-
 	has_many :microposts, dependent: :destroy
-	attr_accessor :remember_token
+	attr_accessor :remember_token, :activation_token
+	before_create :create_activation_digest
 	before_save { self.email = email.downcase}
 	validates :name, presence: true, length: {maximum: 50}
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -33,5 +33,16 @@ def User.digest(string)
  end
  def feed
 	Micropost.where("user_id = ?", id)
+ end
+ private
+ # Creates email to all lower-case
+ def downcase_email
+ 	self.email = email.downcase
+ end
+
+ #Creates and assigns the activation token and digest.
+ def create_activation_digest
+ 	self.activation_token = User.new_token
+ 	self.activation_digest = User.digest(activation_token)
  end
  end
