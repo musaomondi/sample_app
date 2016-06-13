caef825b288a787f818e68e1ac6ce4b0eb312602
musaomondi <amokem@yahoo.com>
2016-06-13 12:59:03 +0300

    solving failure

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -8,19 +8,24 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 
   test "layout links" do
   	get root_path
-  	assert_template 'static_pages/home'
-  	assert_select "a[href=?]", root_path, count: 2
-	assert_select "a[href=?]", help_path
-	assert_select "a[href=?]", about_path
-	assert_select "a[href=?]", contact_path
-	get signup_path
-	assert_select "title", full_title("Sign up")
-	log_in_as(@user)
-	assert_select "a[href=?]", login_path, count: 0
-	assert_select "a[href=?]", logout_path
-	assert_select "a[href=?]", users_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path   
+    assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", login_path
+    
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
+
+  	get signup_path
+  	assert_select "title", full_title("Sign up")
 	assert_select 'a[href=?]', '#', text: 'Account'
-	assert_select "a[href=?]", user_path(@user)
-	assert_select "a[href=?]", edit_user_path(@user)
+	
 end
 end
