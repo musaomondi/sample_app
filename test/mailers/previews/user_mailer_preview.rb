904f287f27a2068135422e226bc4f1b7e60371ad
musaomondi <amokem@yahoo.com>
2016-06-13 10:51:09 +0300

    update authenticated? method

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -14,3 +14,4 @@ class UserMailerPreview < ActionMailer::Preview
   end
 
 end
+
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -46,7 +46,7 @@ class UserTest < ActiveSupport::TestCase
     @user.password =@user.password_confirmation = "a"*5
   end
   test "authenticated? should return false for a user with nil digest" do
-    assert_not @user.authenticated? (' ')
+    assert_not @user.authenticated? (:remember, ' ')
   end
   test "associated microposts should be destroyed" do
     @user.save
