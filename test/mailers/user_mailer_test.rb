7cc90d8f2e6ca33a52711713819b269380b0b580
musaomondi <amokem@yahoo.com>
2016-06-13 08:07:27 +0300

    configure text file with proper domain name to pass the test

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -7,8 +7,8 @@ class UserMailerTest < ActionMailer::TestCase
     mail = UserMailer.account_activation(user)
     assert_equal "Account activation", mail.subject
     assert_equal [user.email],mail.to
-    assert_equal ["noreply@example.org"], mail.from
-    assert_match user.nsme, mail.body.encoded
+    assert_equal ["noreply@example.com"], mail.from
+    assert_match user.name, mail.body.encoded
     assert_match user.activation_token, mail.body.encoded
     assert_match CGI::escape(user.email), mail.body.encoded
   end
@@ -17,7 +17,7 @@ class UserMailerTest < ActionMailer::TestCase
     mail = UserMailer.password_reset
     assert_equal "Password reset", mail.subject
     assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
+    assert_equal ["noreply@example.com"], mail.from
     assert_match "Hi", mail.body.encoded
   end
 
