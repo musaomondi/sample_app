f3a9784252dda7925b8222b38ed2fd4ce525e7d1
musaomondi <amokem@yahoo.com>
2016-06-14 09:45:25 +0300

    migration to add password reset attributes

---
+++ db/migrate/20160614064223_add_reset_to.rb
@@ -0,0 +1,4 @@
+class AddResetTo < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160613025031) do
+ActiveRecord::Schema.define(version: 20160614064223) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
