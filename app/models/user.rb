c5e5cfccf5a2fc50783cec3bd6b036c9ea89d830
musaomondi <amokem@yahoo.com>
2016-06-14 10:19:20 +0300

    adding password reset methods to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
 
 	has_many :microposts
 	has_many :microposts, dependent: :destroy
-	attr_accessor :remember_token, :activation_token
+	attr_accessor :remember_token, :activation_token, :reset_token
 	before_create :create_activation_digest
 	before_save { self.email = email.downcase}
 	validates :name, presence: true, length: {maximum: 50}
@@ -43,6 +43,16 @@ def User.digest(string)
  #Sends activation email
  def send_activation_email
  	UserMailer.account_activation(self).deliver_now
+ end
+ # Sets the password reset attributes
+ def create_reset_digest
+ 	self.reset_token = User.new_token
+ 	update_attribute(:reset_digest, User.digest(reset_token))
+ 	update_attribute(:reset_sent_at, Time.zone.now)
+ end
+ #Sends password reset email
+ def send_password_reset_email
+ 	UserMailer.password_reset(self).deliver_now
  end
  private
  # Creates email to all lower-case
