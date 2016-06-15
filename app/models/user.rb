4bc59acf40cba581bc52cb1d4123cc34df70a820
musaomondi <amokem@yahoo.com>
2016-06-15 11:59:06 +0300

    complete password reset test

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,12 +47,15 @@ def User.digest(string)
  # Sets the password reset attributes
  def create_reset_digest
  	self.reset_token = User.new_token
- 	update_attribute(:reset_digest, User.digest(reset_token))
- 	update_attribute(:reset_sent_at, Time.zone.now)
+	update_attribute(:reset_digest, User.digest(reset_token))
+	update_attribute(:reset_sent_at, Time.zone.now)
  end
  #Sends password reset email
  def send_password_reset_email
  	UserMailer.password_reset(self).deliver_now
+ end
+ def password_reset_expired?
+ 	reset_sent_at < 2.hours.ago
  end
  private
  # Creates email to all lower-case
