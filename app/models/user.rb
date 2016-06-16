11fd7b7242fd1a9c06c4380c0868f6f8fb71815b
musaomondi <amokem@yahoo.com>
2016-06-16 09:47:27 +0300

    Implement user.followers using passive relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,11 @@ class User < ActiveRecord::Base
 	has_many :active_relationships, class_name: "Relationship",
 									foreign_key: "follower_id",
 									dependent: :destroy
+	has_many :passive_relationships, class_name: "Relationship",
+									 foreign_key: "followed_id",
+									 dependent: :destroy
 	has_many :following, through: :active_relationships, source: :followed
+	has_many :followers, through: :passive_relationships, source: :follower
 	attr_accessor :remember_token, :activation_token
 	before_create :create_activation_digest
 	before_save { self.email = email.downcase}
@@ -50,11 +54,11 @@ def User.digest(string)
  end
  #Follows a user
  def follow(other_user)
- 	active_relationships.create(followed_id: other_user_id)
+	active_relationships.create(followed_id: other_user.id)
  end
  #Unfollows a user.
  def unfollow(other_user)
- 	active_relationships.find_by(followed_id: other_user_id).destroy
+ 	active_relationships.find_by(followed_id: other_user.id).destroy
  end
  #Returns true if the cureent user is following the other user.
  def following?(other_user)
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -60,6 +60,8 @@ class UserTest < ActiveSupport::TestCase
     archer = users(:archer)
     assert_not michael.following?(archer)
     michael.follow(archer)
+    assert michael.following?(archer)
+    assert archer.followers.include?(michael)
     michael.unfollow(archer)
     assert_not michael.following?(archer)
   end
