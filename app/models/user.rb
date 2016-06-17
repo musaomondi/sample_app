1664e123a0346c72abc7af8e9d715c21a6e99c76
musaomondi <amokem@yahoo.com>
2016-06-17 13:07:17 +0300

    fix bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
 
-	has_many :microposts
 	has_many :microposts, dependent: :destroy
 	has_many :active_relationships, class_name: "Relationship",
 									foreign_key: "follower_id",
